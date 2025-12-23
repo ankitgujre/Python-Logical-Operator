7dd4bc9e405473ca44bbe0e8d0d20c39f29bf9a6
ankitgujre <ankitgujre374@gmail.com>
2025-12-23 20:17:53 +0530

    ternary operator questions

--- Ternary_operator.py
+++ Ternary_operator.py
@@ -7,6 +7,60 @@
 
 # check the number is even or odd
 
-num = 7
+# num = 7
 
-print("even") if num%2 == 0 else print("odd")
+# print("even") if num%2 == 0 else print("odd")
+
+# check age for vote
+
+# age = int(input("Enter age :"))
+
+# print("eligible for vote ") if age >= 18 else print("not eligible")
+
+# check password correct or not
+
+# pwd = int(input("Enter password: "))
+
+# pss = 123456
+
+# print("correct") if pwd == pss else print("invalid")
+
+# check the email is correct or not
+
+# email = "admin@gmail.com"
+# e = input("enter your email")
+
+# print("correct ") if email == e else print("not correct")
+
+# email = "admin@gmail.com"
+# passw = 123456
+
+# a = input("your email")
+# b = int(input("enter your password"))
+
+# print("email password correct") if email == a and passw == b else print("not match")
+
+# check the number is negative and positive
+
+# num = int(input("enter number: "))
+# print("positive") if num>0 else print("negative")
+
+
+''' check numbe is between 1 - 50 '''
+
+# num = int(input("Enter number "))
+# print("Yes") if num > 1 and num < 50 else print("no")
+
+
+
+'''  check the person pass or fail '''
+
+# marks = int(input("enter marks"))
+
+# print("pass") if marks > 45 else print("fail")
+
+'''   check the number is above 50 and divisible by 3'''
+
+num = int(input("Enter digit: "))
+
+print("numer greater than 50 and divisible by 3") if num > 50 and num %3 == 0 else print("not greater than")
