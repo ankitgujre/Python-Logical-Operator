e1c523707342ae70f98350db4ba0f197e338ab82
ankitgujre <ankitgujre374@gmail.com>
2025-12-23 19:52:05 +0530

    identity membership ternary operators

---
+++ Membership_operator.py
@@ -0,0 +1,4 @@
+# membership operator in and not in
+
+print('p' in "python")
+print('p' not in "python")
---
+++ Ternary_operator.py
@@ -0,0 +1,12 @@
+# greatest between two numbers
+
+# a = 90
+# b = 45
+
+# print(a) if a>b else print(b)
+
+# check the number is even or odd
+
+num = 7
+
+print("even") if num%2 == 0 else print("odd")
